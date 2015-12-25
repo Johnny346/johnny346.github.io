236898641eb1b6e145d25a91f04df06adf2401ed
John <johnnymul346@gmail.com>
2015-12-25 17:04:07 +0000

    add files to online repository

---
+++ js/googlemap.js
@@ -0,0 +1,10 @@
+function initialize() {
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+              center: new google.maps.LatLng(52.66634, -8.637348),
+              zoom: 13,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map(mapCanvas, mapOptions)
+          }
+          google.maps.event.addDomListener(window, 'load', initialize);
